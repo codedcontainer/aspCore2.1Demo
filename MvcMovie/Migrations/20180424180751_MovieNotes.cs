fee82e00c3a05aa16a5f167a17aac2776c0af4c9
Jason Utt <jutt@iupui.edu>
2018-04-25 15:01:38 -0400

    Finished mvc core demo files

---
+++ MvcMovie/Migrations/20180424180751_MovieNotes.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MvcMovie.Migrations
+{
+    public partial class MovieNotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Movie");
+        }
+    }
+}
---
+++ MvcMovie/Migrations/20180425163424_Rating.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Notes",
+                table: "Movie",
+                newName: "Rating");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rating",
+                table: "Movie",
+                newName: "Notes");
+        }
+    }
+}
